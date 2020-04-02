fcc13d62ecfb58d1757ba48806573546d2123427
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-04-02 13:43:50 -0300

    Primeiro commit

---
+++ api/src/controllers/SessionController.js
@@ -0,0 +1,18 @@
+const connection = require('./../database/connection');
+module.exports = {
+    async login(request, response){
+        const { id } = request.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if(!ong){
+            return response.status(400).json({ error: 'ONG NÃO CADASTRADA!' });
+        }
+
+        return response.json(ong);
+
+    }
+}
